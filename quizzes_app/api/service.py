85bdf15b761e06f70aa78668fe70e7652ff5c78d
Maik G <maik.g@outlook.com>
2026-07-22 12:31:37 +0200

    Add audio download functionality using yt_dlp; create service.py for audio extraction

---
+++ quizzes_app/api/service.py
@@ -0,0 +1,19 @@
+
+import yt_dlp
+import os
+
+MEDIA_DIR = "media/temp"
+
+def download_audio(url):
+
+    os.makedirs(MEDIA_DIR, exist_ok=True)
+
+    ydl_opts = { 
+        "format": "bestaudio/best",
+        "outtmpl": MEDIA_DIR + "/%(id)s.%(ext)s",
+        "quiet": True,
+        "noplaylist": True,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        return ydl.prepare_filename(info)
