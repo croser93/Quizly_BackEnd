50ab0bdc0ce764f2c76da635db49c858e2c360da
Maik G <maik.g@outlook.com>
2026-07-22 12:11:34 +0200

    Refactor QuizzesView post method by removing unused imports and exception handling; streamline URL validation response

--- quizzes_app/api/views.py
+++ quizzes_app/api/views.py
@@ -1,8 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import  IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
-from django.contrib.auth import authenticate
 from .serializer import QuizSerializer, URLSerializer
 from ..models import Quiz
 
@@ -11,8 +9,6 @@ class QuizzesView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-
-        try:
             serializer = URLSerializer(data=request.data)
             if serializer.is_valid():
                 quiz = Quiz.objects.create(video_url=serializer.validated_data['url'], user=request.user, title="TODO", description="TODO")
@@ -20,7 +16,5 @@ class QuizzesView(APIView):
                 return Response(quiz_serializer.data, status=201)
             else:
                 return Response({"error": "Ungültige URL oder Anfragedaten."}, status=400)
-        except:
-            pass
 class QuizzesDetailView(APIView):
     pass
