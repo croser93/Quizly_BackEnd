1aebb0e8c26b9c7ab3522580c1df9042f89b27e1
Maik G <maik.g@outlook.com>
2026-07-23 08:42:41 +0200

    Add QuestionSerializer and update QuizSerializer to include questions; modify QuizzesView to create questions from quiz JSON

--- quizzes_app/api/views.py
+++ quizzes_app/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import  IsAuthenticated
 from rest_framework.response import Response
 from .serializer import QuizSerializer, URLSerializer
-from ..models import Quiz
+from ..models import Quiz, Question
 from .service import download_audio
 from .whisper import transcript
 from .gemini import gemini
@@ -16,14 +16,17 @@ class QuizzesView(APIView):
             serializer = URLSerializer(data=request.data)
             if serializer.is_valid():
 
-                audio_path = download_audio(serializer.validated_data['url'])
-                transcr = transcript(audio_path)
+                # audio_path = download_audio(serializer.validated_data['url'])
+                transcr = transcript("media/temp/i3a7B65b6w8.webm")
                 quiz_json = gemini(transcr)
                 quiz_json = json.loads(quiz_json)
 
                 quiz = Quiz.objects.create(video_url=serializer.validated_data['url'], user=request.user, title=quiz_json['title'], description=quiz_json['description'])
-                print(quiz)
+
+                for element in quiz_json['questions']:
+                    question = Question.objects.create(quiz= quiz, question_title = element['question_title'], question_options = element['question_options'], answer = element['answer'])
                 quiz_serializer = QuizSerializer(quiz)
+                print(quiz_serializer.data)
                 return Response(quiz_serializer.data, status=201)
             else:
                 return Response({"error": "Ungültige URL oder Anfragedaten."}, status=400)
