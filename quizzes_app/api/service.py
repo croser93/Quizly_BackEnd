735998fa24bb4d1fa7d0717b88f307d928f91c85
Maik G <maik.g@outlook.com>
2026-07-24 14:09:15 +0200

    Refactor quiz processing: integrate transcription and Gemini request handling in start_quiz_chain function

--- quizzes_app/api/service.py
+++ quizzes_app/api/service.py
@@ -1,6 +1,8 @@
 
 import yt_dlp
 import os
+from .whisper import transcript
+from .gemini import gemini
 
 MEDIA_DIR = "media/temp"
 
@@ -17,3 +19,16 @@ def download_audio(url):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
         return ydl.prepare_filename(info)
+
+def start_quiz_chain(audio_path):
+    try:
+        transcr = transcript(audio_path)
+    except Exception:
+        raise Exception("Transcription failed")   
+    
+    try:
+        from_gemin_json = gemini(transcr)
+    except Exception:
+        raise Exception("Gemini request failed")   
+    return from_gemin_json
+    
--- quizzes_app/api/views.py
+++ quizzes_app/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from .serializer import QuizSerializer, URLSerializer
 from .permission import UserIsCreatorOrAdmin
 from ..models import Quiz, Question
-from .service import download_audio
+from .service import download_audio, start_quiz_chain
 from .whisper import transcript
 from .gemini import gemini
 import json
@@ -31,15 +31,21 @@ class QuizzesView(APIView):
         serializer = URLSerializer(data=request.data)
         if serializer.is_valid():
 
-            audio_path = download_audio(serializer.validated_data['url'])
-            transcr = transcript(audio_path)
-            quiz_json = gemini(transcr)
-            quiz_json = json.loads(quiz_json)
+            try:
+                try:
+                    audio_path = download_audio(serializer.validated_data['url'])
+                except Exception:
+                    raise Exception("Error creating the quiz")
 
+                quiz_json = json.loads(start_quiz_chain(audio_path))
+            except Exception as e:
+                return Response({"error": str(e)}, status=400)
+            
             quiz = Quiz.objects.create(video_url=serializer.validated_data['url'], user=request.user, title=quiz_json['title'], description=quiz_json['description'])
             for element in quiz_json['questions']:
-                question = Question.objects.create(quiz= quiz, question_title = element['question_title'], question_options = element['question_options'], answer = element['answer'])
+                Question.objects.create(quiz= quiz, question_title = element['question_title'], question_options = element['question_options'], answer = element['answer'])
             quiz_serializer = QuizSerializer(quiz)
+            print(quiz_serializer.data)
             return Response(quiz_serializer.data, status=201)
         else:
             return Response({"error": "Ungültige URL oder Anfragedaten."}, status=400)
