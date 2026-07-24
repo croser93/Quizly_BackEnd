28ed0de52c402e18ae6c705ae13815f226f413f7
Maik G <maik.g@outlook.com>
2026-07-24 10:41:44 +0200

    Fix URL path for registration endpoint in urls.py

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -7,13 +7,13 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('register/', RegistrationView.as_view(), name='registration'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('token/refresh/', RefreshCookieView.as_view(), name='token_refresh'),
 
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', RefreshCookieView.as_view(), name='token_refresh'),
     path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
 
